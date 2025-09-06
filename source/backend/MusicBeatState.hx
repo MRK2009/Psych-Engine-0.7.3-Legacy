9334c9d487fa8bf7bcef2d96afda0ec1b96a6343
Rafi <mohamedrafikhan2009@gmail.com>
2025-09-06 21:21:05 +0600

    Oops

--- source/backend/MusicBeatState.hx
+++ source/backend/MusicBeatState.hx
@@ -4,7 +4,7 @@ import flixel.addons.ui.FlxUIState;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.FlxState;
 import backend.PsychCamera;
-import mobile.flixel.input.FlxVirtualPad;
+import mobile.flixel.FlxVirtualPad;
 
 class MusicBeatState extends FlxUIState
 {
